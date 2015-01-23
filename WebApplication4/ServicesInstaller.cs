16e239bf3b44c34c20c3c0706df7c74333f7aa32
Sean Amog <samog@kobo.com>
2015-01-23 16:26:25 -0500

    add everything

---
+++ WebApplication4/ServicesInstaller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using ServiceStack.CacheAccess;
+using ServiceStack.CacheAccess.Providers;
+using ServiceStack.Configuration;
+using ServiceStack.OrmLite;
+using ServiceStack.ServiceInterface;
+using ServiceStack.ServiceInterface.Auth;
+
+namespace WebApplication4
+{
+    public class ServicesInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<ICacheClient>().ImplementedBy<MemoryCacheClient>());
+            //container.Register(Component.For<IUserAuthRepository>().ImplementedBy<InMemoryAuthRepository>());
+            var dbConnectionFactory =
+                new OrmLiteConnectionFactory(HttpContext.Current.Server.MapPath("~/App_Data/data.txt"),
+                    SqliteDialect.Provider);
+            var iUserAuthRepository = new OrmLiteAuthRepository(dbConnectionFactory);
+            container.Register(Component.For<IDbConnectionFactory>().Instance(dbConnectionFactory));
+            container.Register(Component.For<IUserAuthRepository>().Instance(iUserAuthRepository));
+            container.Register(Component.For<MeasuredDataRepository>());
+            var authRepo = (OrmLiteAuthRepository)container.Resolve<IUserAuthRepository>();
+            authRepo.CreateMissingTables();
+
+            //string hash;
+            //string salt;
+            //new SaltedHash().GetHashAndSaltString("pass123", out hash, out salt);
+            //authRepo.CreateUserAuth(new UserAuth
+            //{
+            //    Id = 1,
+            //    DisplayName = "User",
+            //    Email = "user@kobo.com",
+            //    UserName = "user",
+            //    FirstName = "User",
+            //    LastName = "User",
+            //    PasswordHash = hash,
+            //    Salt = salt,
+            //    //Roles = new List<string> { RoleNames.Admin }
+            //    //Permissions = new List<string> {"ViewCurrentStatus"}
+            //}, "pass123");
+        }
+    }
+}
---
+++ WebApplication4/WindsorContainerAdapter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using ServiceStack.Configuration;
+
+namespace WebApplication4
+{
+    public class WindsorContainerAdapter : IContainerAdapter, IDisposable
+    {
+        private readonly IWindsorContainer _container;
+
+        public WindsorContainerAdapter()
+        {
+            _container = new WindsorContainer().Install(FromAssembly.InThisApplication(),
+                FromAssembly.InDirectory(new ApplicationAssemblyFilter()));
+        }
+
+        public T TryResolve<T>()
+        {
+            if (_container.Kernel.HasComponent(typeof(T)))
+            {
+                return (T)_container.Resolve(typeof(T));
+            }
+
+            return default(T);
+        }
+
+        public T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        public void Dispose()
+        {
+            _container.Dispose();
+        } 
+    }
+
+    public class ApplicationAssemblyFilter : AssemblyFilter
+    {
+        public ApplicationAssemblyFilter() 
+            : base(AppDomain.CurrentDomain.BaseDirectory, Assembly.GetExecutingAssembly().GetName().Name + ".*.dll"){}
+    }
+}
